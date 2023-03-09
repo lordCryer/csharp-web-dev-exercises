497dec8efa7ac3380dfa99edb8f38aa92c833200
Dion <lordcryer@yahoo.com>
2023-03-09 15:42:39 -0600

    created a student id list

--- Classes-Part-2/Course.cs
+++ Classes-Part-2/Course.cs
@@ -3,9 +3,26 @@ namespace Classes_Part_2
 {
 	public class Course
 	{
-        public string Topic { get; set; }
-        public Teacher Instructor { get; set; }
-        public List<Student> enrolledStudents { get; set; }
+        public string Title { get; set; }
+        public double Credits { get; set; }
+        public List<string> DaysOfTheWeek { get; set; }
+    {
+        public Dictionary<string, int> Books { get; set; }
+
+        public Dictionary<string, int> StudentRoster { get; set; }
+
+        public Course(string title, double credits, List<string> daysOfWeek, Dictionary<string, int> books, List<Student> studentRoster)
+
+
+        {
+            Title = title;
+            Credits = credits;
+            DaysOfTheWeek = daysOfWeek;
+            Books = books;
+            StudentRoster = studentRoster;
+        }
+        }
+    }
 
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather than
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,27 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+using Classes;
+
+Student whateverTheNewNameIs = new Student("Dion", 111, 1, 4.0);
+
+Console.WriteLine(whateverTheNewNameIs.Name);
+whateverTheNewNameIs.Name = "D";
+Console.WriteLine(whateverTheNewNameIs.Name);
+
+Student rosco = new Student("Rosco", 323);
+
+//foreach (Student stu in whateverTheNewNameIs)
+
+Console.WriteLine(rosco.Name);
+Console.WriteLine(rosco.StudentId);
+Console.WriteLine(rosco.NumberOfCredits);
+Console.WriteLine(rosco.Gpa);
+
+Student joe = new Student ("Joe");
+Student john = new Student("John");
+Student devon = new Student("Devon");
+Student dee = new Student("Dee");
+
+Console.WriteLine(joe.StudentId);
+Console.WriteLine(dee.StudentId);
+Console.WriteLine(john.StudentId);
+Console.WriteLine(devon.StudentId);
